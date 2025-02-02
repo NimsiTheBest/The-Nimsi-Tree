902f5832d6ebbce12350144891f82e5674efb770
NimsiTheBest <mikunlolu@icloud.com>
2025-02-02 14:47:58 +0000

    a

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
-addLayer("m", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+addLayer("p", {
+    name: "memes", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -15,7 +15,7 @@ addLayer("m", {
     exponent: 0.5, // Prestige currency exponent
     gainMult() {
         let mult = new Decimal(1)
-        if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
+        if (hasUpgrade('m', 13)) mult = mult.times(upgradeEffect('m', 13))
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
